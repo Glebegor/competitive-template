7ad1e77ed756002e543817ab8bfc980257142d8a
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-09-01 20:14:49 +0200

    Added another implementation of the seg

--- DataStructures/segmentTree.cpp
+++ DataStructures/segmentTree.cpp
@@ -83,3 +83,80 @@ public:
 //     }
 //     return;
 // }
+
+
+
+// struct segmentTree {
+//     int size;
+//     vl a;
+
+//     void init(int n) {
+//         size = 1;
+//         while (size < n) size *= 2;
+//         a.assign(2 * size, 0LL);
+//     }
+
+//     void set(int i, int v, int x, int lx, int rx) {
+//         if (rx - lx == 1) {
+//             a[x] = v;
+//             return;
+//         }
+//         int m = (lx + rx) / 2;
+//         if (i < m) {
+//             set(i, v, 2 * x + 1, lx, m);
+//         } else {
+//             set(i, v, 2 * x + 2, m, rx);
+//         }
+//         a[x] = a[2 * x + 1] + a[2 * x + 2];
+//     }
+
+//     void set(int i, int v) {
+//         set(i, v, 0, 0, size);
+//     }
+
+//     ll sum(int l, int r, int x, int lx, int rx) {
+//         if (lx >= r || l >= rx) return 0;
+//         if (lx >= l && rx <= r) return a[x];
+//         int m = (lx + rx) / 2;
+//         ll s1 = sum(l, r, 2 * x + 1, lx, m);
+//         ll s2 = sum(l, r, 2 * x + 2, m, rx);
+//         return s1 + s2;
+//     }
+
+//     ll sum(int l, int r) {
+//         return sum(l, r, 0, 0, size);
+//     }
+// };
+
+// void solve() {
+//     int n, m;
+//     cin >> n >> m;
+//     segmentTree sg;
+//     sg.init(n);
+//     for (int i = 0; i < n; i++) {
+//         int x;
+//         cin >> x;
+//         sg.set(i, x);
+//     }
+
+//     while (m--) {
+//         int op;
+//         cin >> op;
+//         if (op == 1) {
+//             int i, v;
+//             cin >> i >> v;
+//             sg.set(i, v);
+//         } else {
+//             int l, r;
+//             cin >> l >> r;
+//             cout << sg.sum(l, r) << '\n';
+//         }
+//     }
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     solve();
+//     return 0;
+// }
